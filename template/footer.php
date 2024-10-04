3d3c9b8fa69e6fe30184035fe8db15b5bd8127b4
ranushka-lakmal <ranulakmal@gmail.com>
2024-10-04 05:31:54 +0300

    add map

--- template/footer.php
+++ template/footer.php
@@ -1,64 +1,84 @@
 <!-- footer.php -->
 <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">The Gallery Cafe</h2>
-              <p>Best and Tasty Foods !</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Opening Hours</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Monday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Tuesday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Wednesday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Thursday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Friday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Saturday: <span>08: - 22:00</span></a></li>
-                <li><a href="#" class="py-2 d-block">Sunday: <span>08: - 22:00</span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Contact Information</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Main street - Nugegoda</a></li>
-                <li><a href="#" class="py-2 d-block">011 123456</a></li>
-                <li><a href="#" class="py-2 d-block">thegallerycafe@gmail.com</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Newsletter</h2>
-              <p>Far far away, behind the word mountains, far from the countries.</p>
-              <form action="#" class="subscribe-form">
-                <div class="form-group">
-                  <span class="icon icon-paper-plane"></span>
-                  <input type="text" class="form-control" placeholder="Subscribe">
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-            <p>
-              Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <i class="icon-heart"aria-hidden="true"> </i> by <a href="#" target="_blank"> The Gallery Cafe</a>
-            </p>
-          </div>
+  <div class="container"> 
+    <div class="row mb-5">
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">The Gallery Cafe</h2>
+          <p>Best and Tasty Foods !</p>
+          <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+          </ul>
         </div>
       </div>
-    </footer>
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Opening Hours</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">Monday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Tuesday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Wednesday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Thursday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Friday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Saturday: <span>08: - 22:00</span></a></li>
+            <li><a href="#" class="py-2 d-block">Sunday: <span>08: - 22:00</span></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Contact Information</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">Main street - Nugegoda</a></li>
+            <li><a href="#" class="py-2 d-block">011 123456</a></li>
+            <li><a href="#" class="py-2 d-block">thegallerycafe@gmail.com</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Newsletter</h2>
+          <p>Far far away, behind the word mountains, far from the countries.</p>
+          <form action="#" class="subscribe-form">
+            <div class="form-group">
+              <span class="icon icon-paper-plane"></span>
+              <input type="text" class="form-control" placeholder="Subscribe">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
 
-    <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="ftco-heading-2 text-center"><span style="color:white">Location Map </span></h2>
+        <!-- Embed Google Maps iframe -->
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198877.30955150047!2d79.89831385662596!3d6.864996707793492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25d52a219d8f3%3A0xc52d5945d3343d9!2sThe%20Gallery%20Cafe!5e0!3m2!1sen!2slk!4v1696276748825!5m2!1sen!2slk"
+          width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy">
+        </iframe>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p>
+          Copyright &copy;
+          <script>document.write(new Date().getFullYear());</script> All rights reserved <i class="icon-heart"
+            aria-hidden="true"> </i> by <a href="#" target="_blank"> The Gallery Cafe</a>
+        </p>
+      </div>
+    </div>
+   
+  </div>
+</footer>
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen">
+  <svg class="circular" width="48px" height="48px">
+    <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+    <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
+  </svg>
+</div>
